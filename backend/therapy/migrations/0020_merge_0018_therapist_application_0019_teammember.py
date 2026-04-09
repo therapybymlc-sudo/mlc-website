8bc8ddba7a576c077cecd4f12b021cf95572adfe
asma <asma.ausa02@gmail.com>
2026-04-09 20:56:40 +0300

    Add merge migration for therapist application and team member

---
+++ backend/therapy/migrations/0020_merge_0018_therapist_application_0019_teammember.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("therapy", "0018_therapist_application"),
+        ("therapy", "0019_teammember"),
+    ]
+
+    operations = []
