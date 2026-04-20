4e0f157638ec095a9a08b78e3b33e169379f8105
asma <asma.ausa02@gmail.com>
2026-04-20 20:58:56 +0300

    Fix: Added missing Chakra UI imports to resolve ReferenceError and 500 on profile page

--- react-frontend/src/app/therapists/[id]/PublicProfileClient.js
+++ react-frontend/src/app/therapists/[id]/PublicProfileClient.js
@@ -25,6 +25,9 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  Spinner,
+  Center,
+  useToast,
 } from "@chakra-ui/react";
 import { 
   FiCheckCircle, FiClock, FiVideo, FiMapPin, FiAward, 
