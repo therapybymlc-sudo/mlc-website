6d2d3e591eba2063fd7523e467bfe689e6bfef9c
asma <asma.ausa02@gmail.com>
2026-04-22 19:24:07 +0300

    fix: robust room name extraction and jaas prefixing for jitsi video

--- react-frontend/src/components/video/TherapyRoom.jsx
+++ react-frontend/src/components/video/TherapyRoom.jsx
@@ -26,7 +26,10 @@ export default function TherapyRoom({ roomUrl, onLeave, jwt }) {
 
   // JaaS Naming Convention: AppID/RoomName
   const JAAS_APP_ID = "vpaas-magic-cookie-0d29cfbee27644b2ad432cdd4f043406";
-  const roomName = roomUrl ? roomUrl.split('/').pop() : "MLC-Secure-Lounge";
+  const rawRoom = roomUrl 
+    ? roomUrl.split('/').filter(Boolean).pop() 
+    : "MLC-Secure-Lounge";
+  const roomName = `${JAAS_APP_ID}/${rawRoom}`;
 
   const handleApiReady = (jitsiApi) => {
     setApi(jitsiApi);
